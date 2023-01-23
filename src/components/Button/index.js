280a9ba1fd49ca804bb09e9441e50ce59685c37a
Nhi Nguyen <nngocnhi555@gmail.com>
2023-01-23 17:46:26 +0700

    layout for 3 button types

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,3 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import classNames from "classnames/bind";
+import styles from "./Button.module.scss";
+
+const cx = classNames.bind(styles);
+
+function Button({ to, href, children, onClick }) {
+  let Component = "button";
+
+  const classes = cx("wrapper");
+
+  return (
+    <Component className={classes}>
+      <span>{children}</span>
+    </Component>
+  );
+}
+
+export default Button;
